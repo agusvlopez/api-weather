b10081d36fc08d7b3e75cbdd962fd57f9fe07a3c
agusvlopez <agusvlopez@gmail.com>
2023-05-19 16:21:00 -0300

    https

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const API_KEY = "033ffc66c5177cc7178a59750e2932ce";
-const URL = "http://api.openweathermap.org/geo/1.0/direct?";
+const URL = "https://api.openweathermap.org/geo/1.0/direct?";
 
 const elementoBotonInput = document.getElementById("btnClima");
 const elementoDatos = document.getElementById('datos');
